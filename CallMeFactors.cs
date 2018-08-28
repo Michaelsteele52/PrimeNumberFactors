3aacc0e7d514e1961371b3a99cd74301e4526e94
Michael George Steele <Michael.Steele@asos.com>
2018-08-28 10:50:30 +0100

    my class how pretty I am

---
+++ CallMeFactors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CallingMePrime
+{
+
+    public class CallMeFactors
+    {
+        public void FindFactors(int factor)
+        {
+            ArrayList listOfFactors = new ArrayList();
+            listOfFactors.Add(factor);
+            for (int i = factor - 1; i > 1; i--)
+            {
+                if (factor % i == 0)
+                {
+                    listOfFactors.Add(i);
+                }
+            }
+            //foreach factor in listOfFactors
+            //{
+                
+            //}
+        }
+    }
+}
